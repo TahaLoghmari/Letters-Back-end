b101bf8a8843645f973443e78346312d61d84c75
Taha Loghmari <medtahalog@gmail.com>
2025-03-23 09:19:28 +0100

    Fixed

--- models/db/pool.js
+++ models/db/pool.js
@@ -7,17 +7,28 @@ if (process.env.NODE_ENV === "production") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false,
+      require: true,
+      rejectUnauthorized: false, 
     },
+    max: 20, 
+    idleTimeoutMillis: 30000,
   });
+
+  pool.on("error", (err) => {
+    console.error("Unexpected error on idle client", err);
+    process.exit(-1);
+  });
+
+  console.log("Connected to production PostgreSQL database on Neon");
 } else {
   pool = new Pool({
-    host: process.env.HOST,
     user: process.env.USER,
+    host: process.env.HOST,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT,
   });
+  console.log("Connected to development PostgreSQL database");
 }
 
 module.exports = pool;
